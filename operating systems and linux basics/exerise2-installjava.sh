e9d0c14dbaf016dac11f6ca97b0bee14a00da7fb
Adeline Graciani <50612536+8deline@users.noreply.github.com>
2025-09-29 12:43:57 +0800

    Create exerise2-installjava.sh

---
+++ operating systems and linux basics/exerise2-installjava.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+apt update
+apt install default-jre
+
+java_version = $(java --version | grep "java version/|openjdk version" | awk '{print substr($3, 1, 2)}' )
+
+if ["$java_version" == ""]
+then
+  echo "Unsuccessful installation"
+elif [$java_version -ge 11 ]
+then 
+  echo "Java version greater than equal to 11 successful"
+else
+  echo "failed to download correct java version"
+
+fi
