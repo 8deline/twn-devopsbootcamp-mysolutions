ed7493b941f02889ba4063dcfd42e624892cab7c
Adeline Graciani <50612536+8deline@users.noreply.github.com>
2025-09-29 14:25:38 +0800

    Update exerise2-installjava.sh

--- operating systems and linux basics/exerise2-installjava.sh
+++ operating systems and linux basics/exerise2-installjava.sh
@@ -3,7 +3,7 @@
 apt update
 apt install -y default-jre
 
-java_version=$(java --version 2>&1 | awk -F\" "/version/ {print $2}" | cut -d. -f1 )
+java_version=$(java --version 2>&1 | awk -F\" '/version/ {print $2}' | cut -d. -f1 )
 
 if [ "$java_version" == "" ]
 then
