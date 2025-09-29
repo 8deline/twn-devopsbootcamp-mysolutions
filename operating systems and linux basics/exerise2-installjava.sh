4bf2bf5276a65bb0b4d4778ee603121f15864c18
Adeline Graciani <50612536+8deline@users.noreply.github.com>
2025-09-29 14:12:57 +0800

    Update exerise2-installjava.sh

--- operating systems and linux basics/exerise2-installjava.sh
+++ operating systems and linux basics/exerise2-installjava.sh
@@ -8,10 +8,10 @@ java_version=$(java --version 2>&1 | grep "java version\|openjdk version" | awk 
 if [ "$java_version" == "" ]
 then
   echo "Unsuccessful installation"
+elif [ "$java_version: == "1." ]
+then 
+  echo "old java versions"
 elif [ "$java_version" -ge 11 ]
 then 
   echo "Java version greater than equal to 11 successful"
-else
-  echo "failed to download correct java version"
-
 fi
