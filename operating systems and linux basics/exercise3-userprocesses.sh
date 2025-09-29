b79a5b71b790a631a6e91f7cbfc4fbb20e07b72d
Adeline Graciani <50612536+8deline@users.noreply.github.com>
2025-09-29 21:08:07 +0800

    Update exercise3-userprocesses.sh

--- operating systems and linux basics/exercise3-userprocesses.sh
+++ operating systems and linux basics/exercise3-userprocesses.sh
@@ -1,3 +1,16 @@
 #! /bin/bash
+read -p "Please specify sort by memory or CPU: " sort_by
+read -p "Please specify how many lines to pring: " lines_to_print
 
-sudo ps aux | grep $USER
+processes_running=$(ps aux)
+
+column_number=3
+
+if [ "$sort_by" == memory ]
+then
+  $column_number=4
+
+$processes_running | sort -k $column_number | head $lines_to_print
+
+
+
